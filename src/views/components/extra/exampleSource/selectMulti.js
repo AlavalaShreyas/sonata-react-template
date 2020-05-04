a854307f3eac398c07a02c38a2fb1525fd45f1dd
namratha-s <namaratha.s@sonata-software.com>
2020-05-04 00:35:42 -0700

    select component changes and live editor integration

--- src/views/components/extra/exampleSource/selectMulti.js
+++ src/views/components/extra/exampleSource/selectMulti.js
@@ -1,22 +1,42 @@
-const SelectMultiSource = `import React, { Component } from "react";
-import Select from "react-select";
-import { colourOptions } from "./selectData";
+const SelectMultiSource = `
+//import React from "react";
+//import Select from "react-select";
 
-class MultiSelect extends Component {
-   render() {
-      return (
-         <Select
-            defaultValue={[colourOptions[2], colourOptions[3]]}
-            isMulti
-            name="colors"
-            options={colourOptions}
-            className="basic-multi-select"
-            classNamePrefix="select"
-         />
-      );
-   }
+class MultiSelect extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedOption: [],
+    };
+  }
+  onSelectChanged(value) {
+    this.setState({
+      selectedOption: value,
+    });
+  }
+  render() {
+    const options = [
+      { value: "ocean", label: "Ocean", color: "#00B8D9", isFixed: true },
+      { value: "blue", label: "Blue", color: "#0052CC", disabled: true },
+      { value: "purple", label: "Purple", color: "#5243AA" },
+      { value: "red", label: "Red", color: "#FF5630", isFixed: true },
+      { value: "orange", label: "Orange", color: "#FF8B00" },
+      { value: "yellow", label: "Yellow", color: "#FFC400" },
+    ];
+    return (
+      <Select
+        options={options}
+        className="basic-multi-select"
+        classNamePrefix="select"
+        defaultValue={[options[1], options[2]]}
+        name="colors"
+        isMulti
+        autoFocus={true}
+        onChange={this.onSelectChanged.bind(this)}
+      />
+    );
+  }
 }
-export default MultiSelect;
 `;
 
 export default SelectMultiSource;
--- src/views/components/extra/exampleSource/selectSingle.js
+++ src/views/components/extra/exampleSource/selectSingle.js
@@ -1,23 +1,41 @@
-const SelectSingleSource = `import React, { Component } from "react";
+const SelectSingleSource = `
+//import React, { Component } from "react";
+//import Select from "react-select";
 
-import Select from "react-select";
-import { colourOptions } from "./selectData";
-
-class SingleSelect extends Component {
-
-   render() {
-      return (
-         <Select
-            className="basic-single"
-            classNamePrefix="select"
-            defaultValue={colourOptions[0]}
-            name="color"
-            options={colourOptions}
-         />
-      );
-   }
+class SingleSelect extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedOption: [],
+    };
+  }
+  onSelectChanged(value) {
+    this.setState({
+      selectedOption: value,
+    });
+  }
+  render() {
+    const options = [
+      { value: "ocean", label: "Ocean", color: "#00B8D9", isFixed: true },
+      { value: "blue", label: "Blue", color: "#0052CC", disabled: true },
+      { value: "purple", label: "Purple", color: "#5243AA" },
+      { value: "red", label: "Red", color: "#FF5630", isFixed: true },
+      { value: "orange", label: "Orange", color: "#FF8B00" },
+      { value: "yellow", label: "Yellow", color: "#FFC400" }
+    ];
+    return (
+      <Select
+        options={options}
+        className="basic-select"
+        classNamePrefix="select"
+        defaultValue={options[0]}
+        name="colors"
+        autoFocus={true}
+        onChange={this.onSelectChanged.bind(this)}
+      />
+    );
+  }
 }
-export default SingleSelect;
 `;
 
 export default SelectSingleSource;
