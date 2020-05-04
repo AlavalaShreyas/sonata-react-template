afe31b7d26f18ef1445fffa086e306be9c397132
MachunuruSwetha <64771226+MachunuruSwetha@users.noreply.github.com>
2020-05-04 22:11:52 +0530

    Button variant components (#11)

--- src/views/components/reactstrap/examples/buttonsSizing.js
+++ src/views/components/reactstrap/examples/buttonsSizing.js
@@ -1,28 +1,14 @@
 import React from "react";
 import { Button } from "reactstrap";
 
-class ButtonsSizing extends React.Component {
-   render() {
-      return (
-         <div>
-            <h5>Large Size</h5>
-            <div className="btn-toolbar">
-               <Button color="primary" size="lg">primary</Button>{" "}
-               <Button color="success" size="lg">success</Button>{" "}
-               <Button color="info" size="lg">info</Button>{" "}
-               <Button color="danger" size="lg">danger</Button>{" "}
-            </div>
+const ButtonSizing = ({ iconType = "Primary", color = "primary", size = "lg", buttonText = "Primary" }) => {
+  const compositeButtonSizing = {
+    "Primary": (<Button color={color} size={size} >{buttonText}</Button>),
+    "Success": (<Button color={color} size={size} >{buttonText}</Button>),
+    "Info": (<Button color={color} size={size} >{buttonText}</Button>),
+    "Danger": (<Button color={color} size={size} >{buttonText}</Button>)
+  };
+  return compositeButtonSizing[iconType];
+};
 
-            <h5>Small Size</h5>
-            <div className="btn-toolbar">
-               <Button color="primary" size="sm">primary</Button>{" "}
-               <Button color="success" size="sm">success</Button>{" "}
-               <Button color="info" size="sm">info</Button>{" "}
-               <Button color="danger" size="sm">danger</Button>{" "}
-            </div>
-         </div>
-      );
-   }
-}
-
-export default ButtonsSizing;
+export default ButtonSizing;
