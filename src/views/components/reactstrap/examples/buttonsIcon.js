5444a32757d6e201a03fa49bb270f3cac7779094
swetha.machunuru <swetha.machunuru@sonata-software.com>
2020-05-04 19:49:05 +0530

    Button variant components

--- src/views/components/reactstrap/examples/buttonsIcon.js
+++ src/views/components/reactstrap/examples/buttonsIcon.js
@@ -1,61 +1,49 @@
 import React from "react";
 import { Button } from "reactstrap";
 import {
-   Settings,
-   Info,
-   ThumbsUp,
-   AlertOctagon,
-   AlertTriangle
+  Settings,
+  Info,
+  ThumbsUp,
+  AlertOctagon,
+  AlertTriangle,
 } from "react-feather";
 
-class ButtonsIcon extends React.Component {
-   render() {
-      return (
-         <div>
-            <div className="btn-toolbar">
-               <Button color="primary">
-                  <Settings size={16} color="#FFF" /> primary
-               </Button>{" "}
-               <Button color="secondary">
-                  <Settings size={16} color="#FFF" /> secondary
-               </Button>{" "}
-               <Button color="success">
-                  <ThumbsUp size={16} color="#FFF" /> success
-               </Button>{" "}
-               <Button color="info">
-                  info <Info size={16} color="#FFF" />{" "}
-               </Button>{" "}
-               <Button color="warning">
-                  warning <AlertOctagon size={16} color="#FFF" />
-               </Button>{" "}
-               <Button color="danger">
-                  danger <AlertTriangle size={16} color="#FFF" />
-               </Button>{" "}
-            </div>
-            <div className="btn-toolbar">
-               <Button outline color="primary">
-                  <Settings size={16} color="#009DA0" /> primary
-               </Button>{" "}
-               <Button outline color="secondary">
-                  <Settings size={16} color="#868e96" /> secondary
-               </Button>{" "}
-               <Button outline color="success">
-                  <ThumbsUp size={16} color="#0CC27E" />
-                  success
-               </Button>{" "}
-               <Button outline color="info">
-                  info <Info size={16} color="#1CBCD8" />
-               </Button>{" "}
-               <Button outline color="warning">
-                  warning <AlertOctagon size={16} color="#FF8D60" />
-               </Button>{" "}
-               <Button outline color="danger">
-                  danger <AlertTriangle size={16} color="#FF586B" />
-               </Button>
-            </div>
-         </div>
-      );
-   }
-}
+const ButtonsIcon = ({
+  iconType = "Settings",
+  iconSize = "16",
+  buttonType = "primary",
+  iconColor = "#FFF",
+  buttonText = "Primary",
+}) => {
+  const compositeButton = {
+    Settings: (
+      <Button color={buttonType}>
+        <Settings size={iconSize} color={iconColor} /> {buttonText}
+      </Button>
+    ),
+    Info: (
+      <Button color={buttonType}>
+        <Info size={iconSize} color={iconColor} /> {buttonText}
+      </Button>
+    ),
+    ThumbsUp: (
+      <Button color={buttonType}>
+        <ThumbsUp size={iconSize} color={iconColor} /> {buttonText}
+      </Button>
+    ),
+    AlertOctagon: (
+      <Button color={buttonType}>
+        {buttonText} <AlertOctagon size={iconSize} color={iconColor} />
+      </Button>
+    ),
+    AlertTriangle: (
+      <Button color={buttonType}>
+        {buttonText} <AlertTriangle size={iconSize} color={iconColor} />
+      </Button>
+    ),
+  };
+
+  return compositeButton[iconType];
+};
 
 export default ButtonsIcon;
