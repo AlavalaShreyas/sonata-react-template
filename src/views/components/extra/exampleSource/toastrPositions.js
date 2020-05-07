de1d98b176613ed8fa613338027f5391a8ef6b42
abhishekbs <abhibscode@gmail.com>
2020-05-07 21:00:16 +0530

    Integrated custom Toaster component

--- src/views/components/extra/exampleSource/toastrPositions.js
+++ src/views/components/extra/exampleSource/toastrPositions.js
@@ -1,51 +1,18 @@
-const toastrPositionsSource = `import React, { Component } from "react";
-import {toastr} from 'react-redux-toastr';
-import { Row, Col, Button } from "reactstrap";
+const toastrPositionsSource = `
 
-class ToastrPositions extends Component {
+class ToastrPositions extends React.Component {
    render() {
       return (
-         <Row>
-            <Col sm="12" md="6" lg="4">
-                <Button outline block color="danger" onClick={() => toastr.success('The title', 'The message', { position: 'top-left'})}>
-                    Top Left
-                </Button>{" "}
-            </Col>
-            
-            <Col sm="12" md="6" lg="4">
-                <Button outline block color="danger" onClick={() => toastr.info('The title', 'The message', { position: 'top-center'})}>
-                    Top Center
-                </Button>{" "}
-            </Col>
-            
-            <Col sm="12" md="6" lg="4">
-                <Button outline block color="danger" onClick={() => toastr.success('The title', 'The message', { position: 'top-right'})}>
-                    Top Right
-                </Button>{" "}
-            </Col>
-            
-            <Col sm="12" md="6" lg="4">
-                <Button outline block color="danger" onClick={() => toastr.info('The title', 'The message', { position: 'bottom-left'})}>
-                    Bottom Left
-                </Button>{" "}
-            </Col>
-            
-            <Col sm="12" md="6" lg="4">
-                <Button outline block color="danger" onClick={() => toastr.success('The title', 'The message', { position: 'bottom-center'})}>
-                    Bottom Center
-                </Button>{" "}
-            </Col>
-            
-            <Col sm="12" md="6" lg="4">
-                <Button outline block color="danger" onClick={() => toastr.info('The title', 'The message', { position: 'bottom-right'})}>
-                    Bottom Right
-                </Button>{" "}
-            </Col>
-         </Row>
+         <Toaster
+         buttonText="Launch Toaster"
+         buttonType="primary"
+         className="success my-2 rounded"
+         toastrType="success"
+         toastrHeaderText="Toaster HeaderText"
+         toastrMessageText="This is a toast message — check it out!"
+         />
       );
    }
-}
-
-export default ToastrPositions;`;
+}`;
 
 export default toastrPositionsSource;
